a94fc38ae664613dbb8e117332c50fd00e195f16
Ajani Timothy <timothyinioluwa@gmail.com>
2022-03-18 06:18:30 +0100

    added static storage

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -67,10 +67,12 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default':dj_database_url.config()
+DATABASES =  {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
